5c6b46344c4aa1c759da263530529a15bee5502c
BuiDinhVan <dinhvanvtpt123@gmail.com>
2022-11-28 23:26:40 +0700

    van update routers products.js

--- routes/products.js
+++ routes/products.js
@@ -1,11 +1,11 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
 
-const { default: mongoose } = require('mongoose');
-const { Product } = require('../models');
+const { default: mongoose } = require("mongoose");
+const { Product } = require("../models");
 
 //MONGOOSE
-mongoose.connect('mongodb://127.0.0.1:27017/cloud-market-AH');
+mongoose.connect("mongodb://127.0.0.1:27017/cloud-market-AH");
 
 //MONGODB
 // const { findDocuments } = require('../helpers/MongoDbHelper');
@@ -13,12 +13,12 @@ mongoose.connect('mongodb://127.0.0.1:27017/cloud-market-AH');
 //============================BEGIN MONGOOSE============================//
 
 /* GET data Products. */
-router.get('/', function (req, res, next) {
+router.get("/", function (req, res, next) {
   try {
     Product.find()
       .lean({ virtuals: true })
-      .populate('category')
-      .populate('supplier')
+      .populate("category")
+      .populate("supplier")
       .then((result) => {
         res.send(result);
       });
@@ -28,13 +28,30 @@ router.get('/', function (req, res, next) {
   }
 });
 
-// GET data Product
-router.get('/:id', function (req, res, next) {
-  const getId = req.params.id;
-  if (getId === 'search') {
+/* GET data Products by categoryId. */
+router.get("/:categoryId", function (req, res, next) {
+  console.log(req.params);
+  const categoryId = req.params.categoryId;
+  if (categoryId === "search") {
     next();
     return;
   }
+  try {
+    Product.find({ categoryId })
+      .lean({ virtuals: true })
+      .populate("category")
+      .then((result) => {
+        res.send(result);
+      });
+  } catch (error) {
+    console.log(error);
+    res.sendStatus(500);
+  }
+});
+// GET data Product
+router.get("/:categoryId/:id", function (req, res, next) {
+  const getId = req.params.id;
+
   try {
     const id = req.params.id;
     Product.findById(id).then((result) => {
@@ -50,14 +67,14 @@ router.get('/:id', function (req, res, next) {
 });
 
 // Search Product
-router.get('/search', (req, res, next) => {
+router.get("/search", (req, res, next) => {
   const { id, firstName, lastName } = req.query;
   console.log(`id: ${id}`);
-  res.send('OK query string');
+  res.send("OK query string");
 });
 
 //Insert Product
-router.post('/', (req, res, next) => {
+router.post("/", (req, res, next) => {
   try {
     const data = req.body;
     const newItem = new Product(data);
@@ -72,7 +89,7 @@ router.post('/', (req, res, next) => {
 });
 
 //Update Product
-router.patch('/:id', (req, res, next) => {
+router.patch("/:id", (req, res, next) => {
   try {
     const id = req.params.id;
     const data = req.body;
@@ -91,7 +108,7 @@ router.patch('/:id', (req, res, next) => {
 });
 
 //Remove Product
-router.delete('/:id', (req, res, next) => {
+router.delete("/:id", (req, res, next) => {
   try {
     const { id } = req.params;
     Product.findByIdAndDelete(id).then((result) => {
