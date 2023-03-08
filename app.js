1929199b2347eaaf6eae158a4a562b3e5af694fd
DucNguyen <nguyenngocduc94751@gmail.com>
2023-03-08 10:00:47 +0700

    duc upload deploy

--- app.js
+++ app.js
@@ -67,6 +67,7 @@ app.use(
       "http://localhost:5173",
       "http://127.0.0.1:5173",
       "https://cloud-market-at-home.vercel.app/",
+      "https://management-cloud-market-at-home.vercel.app/",
     ],
     methods: "GET,POST,PATCH,DELETE,PUT",
     credentials: true,
