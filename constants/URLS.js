89ae30388d58b79d6b6ae151ca042508811e63e2
DucNguyen <nguyenngocduc94751@gmail.com>
2023-04-25 08:33:27 +0700

    update image orders

--- constants/URLS.js
+++ constants/URLS.js
@@ -1,11 +1,11 @@
-// const API_URL = "http://localhost:9000";
-const API_URL = "https://be-cloud-market-at-home.onrender.com";
+const API_URL = "http://localhost:9000";
+// const API_URL = "https://be-cloud-market-at-home.onrender.com";
 
-// const FRONTLINE_URL = "http://localhost:3000";
-const FRONTLINE_URL = "https://cloud-market-at-home.vercel.app";
+const FRONTLINE_URL = "http://localhost:3000";
+// const FRONTLINE_URL = "https://cloud-market-at-home.vercel.app";
 
-// const MANAGEMENT_URL = "http://localhost:5173";
-const MANAGEMENT_URL = "https://management-cloud-market-at-home.vercel.app";
+const MANAGEMENT_URL = "http://localhost:5173";
+// const MANAGEMENT_URL = "https://management-cloud-market-at-home.vercel.app";
 
 module.exports = {
   API_URL,
